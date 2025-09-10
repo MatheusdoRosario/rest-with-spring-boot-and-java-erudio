35dc5618327f232d61904b9e3b22ae8b936ef430
MatheusDoRosario <matheusxdargws@gmail.com>
2025-09-10 15:08:43 -0300

    'Continuous Deployment with Github, Github Actions, Google Artifact Registry and Google Cloud Run'

---
+++ 31_DeployToGoogleCloudPlataform/rest-with-spring-boot-and-java-erudio/src/test/java/br/com/erudio/integrationtests/controllers/withyaml/AuthControllerYamlTest.java
@@ -0,0 +1,93 @@
+package br.com.erudio.integrationtests.controllers.withyaml;
+
+import br.com.erudio.config.TestConfigs;
+import br.com.erudio.data.dto.security.TokenDTO;
+import br.com.erudio.integrationtests.controllers.withyaml.mapper.YAMLMapper;
+import br.com.erudio.integrationtests.dto.AccountCredentialsDTO;
+import br.com.erudio.integrationtests.testcontainers.AbstractIntegrationTest;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.dataformat.xml.XmlMapper;
+import io.restassured.config.EncoderConfig;
+import io.restassured.config.RestAssuredConfig;
+import io.restassured.http.ContentType;
+import org.junit.jupiter.api.*;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+
+import static io.restassured.RestAssured.given;
+import static org.junit.Assert.assertNotNull;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+class AuthControllerYamlTest extends AbstractIntegrationTest {
+
+    private static YAMLMapper objectMapper;
+    private static TokenDTO tokenDTO;
+
+    @BeforeAll
+    static void setUp() {
+        objectMapper = new YAMLMapper();
+        tokenDTO = new TokenDTO();
+    }
+
+    @Test
+    @Order(1)
+    void signin() throws JsonProcessingException {
+        AccountCredentialsDTO credentials =
+                new AccountCredentialsDTO("leandro", "admin123");
+
+        tokenDTO = given().config(
+                        RestAssuredConfig.config()
+                                .encoderConfig(
+                                        EncoderConfig.encoderConfig().
+                                                encodeContentTypeAs(MediaType.APPLICATION_YAML_VALUE, ContentType.TEXT))
+                )
+                .basePath("auth/signin")
+                    .port(TestConfigs.SERVER_PORT)
+                .contentType(MediaType.APPLICATION_YAML_VALUE)
+                .accept(MediaType.APPLICATION_YAML_VALUE)
+                .body(credentials, objectMapper)
+                .when()
+                    .post()
+                .then()
+                    .statusCode(200)
+                .contentType(MediaType.APPLICATION_YAML_VALUE)
+                .extract()
+                    .body()
+                        .as(TokenDTO.class, objectMapper);
+
+
+        assertNotNull(tokenDTO.getAccessToken());
+        assertNotNull(tokenDTO.getRefreshToken());
+    }
+
+    @Test
+    @Order(2)
+    void refreshToken() throws JsonProcessingException {
+
+        tokenDTO = given().config(
+                        RestAssuredConfig.config()
+                                .encoderConfig(
+                                        EncoderConfig.encoderConfig().
+                                                encodeContentTypeAs(MediaType.APPLICATION_YAML_VALUE, ContentType.TEXT))
+                )
+                .basePath("auth/refresh")
+                .port(TestConfigs.SERVER_PORT)
+                .contentType(MediaType.APPLICATION_YAML_VALUE)
+                .accept(MediaType.APPLICATION_YAML_VALUE)
+                .pathParam("username", tokenDTO.getUsername())
+                .header(TestConfigs.HEADER_PARAM_AUTHORIZATION, "Bearer " + tokenDTO.getRefreshToken())
+                .when()
+                .put("{username}")
+                .then()
+                .statusCode(200)
+                .contentType(MediaType.APPLICATION_YAML_VALUE)
+                .extract()
+                        .body()
+                            .as(TokenDTO.class, objectMapper);
+
+        assertNotNull(tokenDTO.getAccessToken());
+        assertNotNull(tokenDTO.getRefreshToken());
+    }
+}
